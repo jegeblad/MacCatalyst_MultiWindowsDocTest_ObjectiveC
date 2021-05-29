64481100012453361c9e53b9cf22264651707a77
Jens Egeblad <jegeblad@gmail.com>
2021-05-29 14:06:42 +0200

    Forgotten files

---
+++ MultiWindowDocTest/Constants.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/* Some shared constants */
+@interface Constants : NSObject
+
++(NSString*) userActivityTypeDocumentView;
++(NSString*) userActivityTypeOpenRecentDocument;
++(NSString*) userActivityTypeDocumentBrowser;
+
++(NSString*) keyURLData;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MultiWindowDocTest/Constants.m
@@ -0,0 +1,29 @@
+#import "Constants.h"
+
+@implementation Constants
+
+
++(NSString*) userActivityTypeDocumentView
+{
+	return @"com.mexircus.docview";
+}
+
+
++(NSString*) userActivityTypeOpenRecentDocument
+{
+	return @"com.mexircus.openrecentdoc";
+}
+
+
++(NSString*) userActivityTypeDocumentBrowser
+{
+	return @"com.mexircus.docbrowser";
+}
+
++(NSString*) keyURLData
+{
+	return @"urldata";
+}
+
+
+@end
